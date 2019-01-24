55a5959f71b06fd949e7b36976e766d972e009bd
Jonathan Rowell <rowell.jonathan@yahoo.com>
2019-01-23 23:47:39 -0600

    adding scrolling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
+import Scroll from './Scroll'
 import './App.css'
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+        
     }
 
     onSearchChange = (event) => {
@@ -25,7 +32,9 @@ class App extends Component {
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
         );
     }
